fb15802412f8ec2ed5fa1cedcb09a4dd56a78889
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2015-01-02 02:02:58 +0700

    tambah contoh 17 ng-mousedown dan mouseup

---
+++ 17_ng_mouse_down_up/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('app', []);
+
+app.controller('MouseDownController', ['$scope', function($scope)
+{
+	$scope.mousedown = {
+		fired: false
+	}
+
+	$scope.mouseup = {
+		fired: false
+	}
+
+	$scope.mouseDown = function()
+	{
+		$scope.mousedown.fired = true;
+		$scope.mouseup.fired = false;
+	}
+
+	$scope.mouseUp = function()
+	{
+		$scope.mouseup.fired = true;
+		$scope.mousedown.fired = false;
+	}
+}]);
