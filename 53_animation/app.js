1c312ab8506377588bc0660652d0653c53ad040f
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2015-01-29 09:29:07 +0700

    tambah contoh 53 ng-animate

---
+++ 53_animation/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('app', ['ngAnimate']);
+
+app.controller('AnimationController', ['$scope', function($scope)
+{
+    $scope.siswa   = ['Andien', 'Heru', 'Ibnu', 'Raisa'];
+}]);
