a43dac0aa18f94d49191036494d9521702839151
Heru Rusdianto <heru.rusdianto.93@gmail.com>
2015-03-12 15:50:29 +0700

    tambah contoh 79 is date

---
+++ 79_is_date/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('app', []);
+
+app.controller('IsArrayController', ['$scope', function($scope)
+{
+    var string = '12-03-2015';
+
+    console.log('string:', string);
+    console.log('is date?', angular.isDate(string));
+
+    var date = new Date(2015, 03, 12);
+
+    console.log('date:', date);
+    console.log('is date?', angular.isDate(date));
+}]);
